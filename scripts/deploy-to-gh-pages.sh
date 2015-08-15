7f7a97154b7b4b101daa651600899e9ed442e6b8
Julien Fouilhé <julien.fouilhe@gmail.com>
2015-08-15 15:50:47 +0200

    Update deploy script

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -3,7 +3,6 @@
 grunt build
 rm -rf /tmp/website-components && mkdir /tmp/website-components && mv node_modules bower_components /tmp/website-components
 git branch -D master
-git push github --delete master
 git checkout --orphan master
 rm -rf /tmp/website-dist && mv dist/ /tmp/website-dist/
 git rm -rf .
@@ -16,6 +15,6 @@ echo "New list"
 ls -l
 git add .
 git commit -m "Deploy website"
-git push github master
+git push github master --force
 git checkout dev
 rm -rf bower_components node_modules && mv /tmp/website-components/* .
