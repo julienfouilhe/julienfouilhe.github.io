1dbfe7422a18bed9e73c9f979f80d7ffb922e087
Julien Fouilhé <julien.fouilhe@gmail.com>
2015-08-15 15:46:55 +0200

    Now master is the branch to be used

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -2,9 +2,9 @@
 
 grunt build
 rm -rf /tmp/website-components && mkdir /tmp/website-components && mv node_modules bower_components /tmp/website-components
-git branch -D gh-pages
-git push github --delete gh-pages
-git checkout --orphan gh-pages
+git branch -D master
+git push github --delete master
+git checkout --orphan master
 rm -rf /tmp/website-dist && mv dist/ /tmp/website-dist/
 git rm -rf .
 mv /tmp/website-dist/* .
@@ -16,6 +16,6 @@ echo "New list"
 ls -l
 git add .
 git commit -m "Deploy website"
-git push github gh-pages
-git checkout master
+git push github master
+git checkout dev
 rm -rf bower_components node_modules && mv /tmp/website-components/* .
